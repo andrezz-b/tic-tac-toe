1d6c9172d4fc47c83c21c0747b323e45debdb706
Andrej Bozic <abozic389@gmail.com>
2021-12-31 18:03:07 +0100

    Formatting

--- scripts/script.js
+++ scripts/script.js
@@ -1,221 +1,226 @@
-const gameBoard = (function(){
-    const board = document.querySelector("#board");
-    const boardFields = Array.from(board.children);
-    let boardArray = [0, 0, 0, 0, 0, 0, 0, 0, 0,];
+const gameBoard = (function () {
+	const board = document.querySelector("#board");
+	const boardFields = Array.from(board.children);
+	let boardArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-    function init(){
-        boardFields.forEach((el, i) => {
-            el.addEventListener("click", populateBoard);
-        })
-    }
+	function init() {
+		boardFields.forEach((el, i) => {
+			el.addEventListener("click", populateBoard);
+		});
+	}
 
-    function render(){
-        boardArray.forEach((el, i) => {
-            let span = document.querySelector(`[data-key="${i}"]`).firstChild;
-            if (el === "x"){
-                span.classList.add("cross");
-            } else if (el === "o"){
-                span.classList.add("circle");
-            } else {
-                span.classList.remove("cross");
-                span.classList.remove("circle");
-            }
-        })
-    }
+	function render() {
+		boardArray.forEach((el, i) => {
+			let span = document.querySelector(`[data-key="${i}"]`).firstChild;
+			if (el === "x") {
+				span.classList.add("cross");
+			} else if (el === "o") {
+				span.classList.add("circle");
+			} else {
+				span.classList.remove("cross");
+				span.classList.remove("circle");
+			}
+		});
+	}
 
-    function resetBoard(){
-        boardArray = [0, 0, 0, 0, 0, 0, 0, 0, 0,];
-        render();
-    }
+	function resetBoard() {
+		boardArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+		render();
+	}
 
-    function populateBoard(e){
-        const div = e.target;
-        const index = parseInt(div.getAttribute("data-key"));
-        if (boardArray[index] === "x" || boardArray[index] === "o") return;
-        boardArray.splice(index, 1, gameController.getTurn());
-        render();
-        if (checkWinCondition() || checkTie()) return;
-        gameController.changeTurn();
-    }
+	function populateBoard(e) {
+		const div = e.target;
+		const index = parseInt(div.getAttribute("data-key"));
+		if (boardArray[index] === "x" || boardArray[index] === "o") return;
+		boardArray.splice(index, 1, gameController.getTurn());
+		render();
+		if (checkWinCondition() || checkTie()) return;
+		gameController.changeTurn();
+	}
 
-    function checkWinCondition(){
-        const winCondition = (gameController.getTurn() === "x") ? "xxx" : "ooo";
-        const rows = [];
-        for (let i = 3; i <= boardArray.length; i += 3) {
-            rows.push(boardArray.slice(i - 3, i));
-        }
+	function checkWinCondition() {
+		const winCondition = gameController.getTurn() === "x" ? "xxx" : "ooo";
+		const rows = [];
+		for (let i = 3; i <= boardArray.length; i += 3) {
+			rows.push(boardArray.slice(i - 3, i));
+		}
 
-        for (let i = 0; i < 3; i++) {
-            let row = "";
-            let column = "";
-            let diag1 = "";
-            let diag2 = "";
-            for (let j = 0; j < 3; j++) {
-                 row += rows[i][j];
-                 column += rows[j][i];
-                 diag1 += rows[j][j];
-                 diag2 += rows[2 - j][j];
-            }
-            if (row === winCondition || column === winCondition || diag1 === winCondition || diag2 === winCondition) {
-                gameController.createOverlay();
-                return 1;
-            }
-        }
-        return 0;
-    }
+		for (let i = 0; i < 3; i++) {
+			let row = "";
+			let column = "";
+			let diag1 = "";
+			let diag2 = "";
+			for (let j = 0; j < 3; j++) {
+				row += rows[i][j];
+				column += rows[j][i];
+				diag1 += rows[j][j];
+				diag2 += rows[2 - j][j];
+			}
+			if (
+				row === winCondition ||
+				column === winCondition ||
+				diag1 === winCondition ||
+				diag2 === winCondition
+			) {
+				gameController.createOverlay();
+				return 1;
+			}
+		}
+		return 0;
+	}
 
-    function checkTie(){
-        const moves = boardArray.filter(el => el != 0);
-        if (moves.length === 9){
-            gameController.createOverlay(true);
-            return 1;
-        }
-        return 0;
-    }
+	function checkTie() {
+		const moves = boardArray.filter((el) => el != 0);
+		if (moves.length === 9) {
+			gameController.createOverlay(true);
+			return 1;
+		}
+		return 0;
+	}
 
-    return {
-        init,
-        resetBoard
-    }
+	return {
+		init,
+		resetBoard,
+	};
 })();
 
-const Player = function(name){
-    let score = 0;
+const Player = function (name) {
+	let score = 0;
 
-    function increaseScore(){
-        score++;
-    }
+	function increaseScore() {
+		score++;
+	}
 
-    function getScore(){
-        return score;
-    }
+	function getScore() {
+		return score;
+	}
 
-    function getSym(){
-        return sym;
-    }
+	function getSym() {
+		return sym;
+	}
 
-    return {
-        name,
-        getScore,
-        getSym,
-        increaseScore,
-    }
-}
+	return {
+		name,
+		getScore,
+		getSym,
+		increaseScore,
+	};
+};
 
-const gameController = (function(){
-    const resetBtn = document.querySelector("#reset-btn");
-    const setPlayer1btn = document.querySelector("#player1-input button");
-    const setPlayer2btn = document.querySelector("#player2-input button");
-    const overlay = document.querySelector(".overlay");
-    const player1 = {name: "Player 1", sym: "o",};
-    const player2 = {name: "Player 2", sym: "x",};
+const gameController = (function () {
+	const resetBtn = document.querySelector("#reset-btn");
+	const setPlayer1btn = document.querySelector("#player1-input button");
+	const setPlayer2btn = document.querySelector("#player2-input button");
+	const overlay = document.querySelector(".overlay");
+	const player1 = { name: "Player 1", sym: "o" };
+	const player2 = { name: "Player 2", sym: "x" };
 
-    let turn = "o";
+	let turn = "o";
 
-    init();
+	init();
 
-    function addListeners(){
-        resetBtn.addEventListener("click", reset);
-        overlay.addEventListener("click", function(){
-            removeOverlay();
-            nextRound(overlay.getAttribute("data-tie"));
-        });
-        setPlayer1btn.addEventListener("click", function(e){
-            reset();
-            createPlayer(e, player1);
-        });
-        setPlayer2btn.addEventListener("click", function(e){
-            reset();
-            createPlayer(e, player2);
-        });
-    }
+	function addListeners() {
+		resetBtn.addEventListener("click", reset);
+		overlay.addEventListener("click", function () {
+			removeOverlay();
+			nextRound(overlay.getAttribute("data-tie"));
+		});
+		setPlayer1btn.addEventListener("click", function (e) {
+			reset();
+			createPlayer(e, player1);
+		});
+		setPlayer2btn.addEventListener("click", function (e) {
+			reset();
+			createPlayer(e, player2);
+		});
+	}
 
-    function init(){
-        createPlayer(undefined, player1);
-        createPlayer(undefined, player2);
-        gameBoard.init();
-    }
+	function init() {
+		createPlayer(undefined, player1);
+		createPlayer(undefined, player2);
+		gameBoard.init();
+	}
 
-    function reset(){
-        overlay.style.display = "none";
-        gameBoard.resetBoard();
-        createPlayer(undefined, player1);
-        createPlayer(undefined, player2);
-        updateScore();
-        turn = "o";
-    }
+	function reset() {
+		overlay.style.display = "none";
+		gameBoard.resetBoard();
+		createPlayer(undefined, player1);
+		createPlayer(undefined, player2);
+		updateScore();
+		turn = "o";
+	}
 
-    function createOverlay(tie){
-        overlay.style.display = "flex";
-        if (tie) {
-            overlay.firstChild.textContent = "Tie";
-            overlay.setAttribute("data-tie", 1);
-        } else {
-            if (getTurn() === "o"){
-                overlay.firstChild.textContent = `${player1.name} wins!`;
-            } else {
-                overlay.firstChild.textContent = `${player2.name} wins!`;
-            }
-        }
-    }
+	function createOverlay(tie) {
+		overlay.style.display = "flex";
+		if (tie) {
+			overlay.firstChild.textContent = "Tie";
+			overlay.setAttribute("data-tie", 1);
+		} else {
+			if (getTurn() === "o") {
+				overlay.firstChild.textContent = `${player1.name} wins!`;
+			} else {
+				overlay.firstChild.textContent = `${player2.name} wins!`;
+			}
+		}
+	}
 
-    function removeOverlay(){
-        overlay.style.display = "none";
-    }
+	function removeOverlay() {
+		overlay.style.display = "none";
+	}
 
-    function getTurn(){
-        return turn;
-    }
-    function changeTurn(){
-        turn = (turn === "x") ? "o" : "x";
-    }
+	function getTurn() {
+		return turn;
+	}
+	function changeTurn() {
+		turn = turn === "x" ? "o" : "x";
+	}
 
-    function createPlayer(event, player){
-        let name = player.name;
-        if (event != undefined){
-            const input = event.target.previousElementSibling.firstElementChild;
-            name = input.value;
-            input.value = "";
-        }
-        player = Object.assign(player, Player(name));
-        setName(player);
-    }
+	function createPlayer(event, player) {
+		let name = player.name;
+		if (event != undefined) {
+			const input = event.target.previousElementSibling.firstElementChild;
+			name = input.value;
+			input.value = "";
+		}
+		player = Object.assign(player, Player(name));
+		setName(player);
+	}
 
-    function setName(player){
-        let className = (player.sym === "o") ? ".player1" : ".player2";
-        const playerElements = document.querySelectorAll(className);
-        playerElements.forEach(el => {
-            el.textContent = player.name;
-        })
-    }
+	function setName(player) {
+		let className = player.sym === "o" ? ".player1" : ".player2";
+		const playerElements = document.querySelectorAll(className);
+		playerElements.forEach((el) => {
+			el.textContent = player.name;
+		});
+	}
 
-    function nextRound(tie){
-        gameBoard.resetBoard();
-        if (!tie) {
-            if (getTurn() === "o"){
-                player1.increaseScore();
-            } else {
-                player2.increaseScore();
-            }
-        }
-        updateScore();
-        turn = "o";
-    } 
+	function nextRound(tie) {
+		gameBoard.resetBoard();
+		if (!tie) {
+			if (getTurn() === "o") {
+				player1.increaseScore();
+			} else {
+				player2.increaseScore();
+			}
+		}
+		updateScore();
+		turn = "o";
+	}
 
-    function updateScore(){
-        const player1Score = document.querySelector("#counter-player1");
-        const player2Score = document.querySelector("#counter-player2");
-        player1Score.textContent = player1.getScore();
-        player2Score.textContent = player2.getScore();
-    }
+	function updateScore() {
+		const player1Score = document.querySelector("#counter-player1");
+		const player2Score = document.querySelector("#counter-player2");
+		player1Score.textContent = player1.getScore();
+		player2Score.textContent = player2.getScore();
+	}
 
-    return {
-        addListeners,
-        createOverlay,
-        nextRound,
-        getTurn,
-        changeTurn,
-    }
+	return {
+		addListeners,
+		createOverlay,
+		nextRound,
+		getTurn,
+		changeTurn,
+	};
 })();
 
 gameController.addListeners();
