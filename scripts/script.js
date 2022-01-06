5426bf38a190b72d08b0b83585371a7f65628af4
Andrej Bozic <abozic389@gmail.com>
2022-01-06 12:50:53 +0100

    Winner of the round is stored in a variable. Added another module for controlling the display.

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,10 @@ const gameBoard = (function () {
 	const board = document.querySelector("#board");
 	const boardFields = Array.from(board.children);
 	let boardArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+	let winner = 0;
 
 	function init() {
-		boardFields.forEach((el, i) => {
+		boardFields.forEach((el) => {
 			el.addEventListener("click", populateBoard);
 		});
 	}
@@ -25,6 +26,7 @@ const gameBoard = (function () {
 
 	function resetBoard() {
 		boardArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+		winner = 0;
 		render();
 	}
 
@@ -37,13 +39,18 @@ const gameBoard = (function () {
 
 		boardArray.splice(index, 1, gameController.getTurn());
 		render();
-		if (checkWinCondition() || checkTie()) return;
+		checkWinner();
+		if (winner) return;
 		gameController.changeTurn();
 	}
 
-	function checkWinCondition() {
-		const winCondition = gameController.getTurn() === "x" ? "xxx" : "ooo";
+	function checkWinner() {
+		const winCondition = (gameController.getTurn() === "x") ? "xxx" : "ooo";
+		const player = (gameController.getTurn() === "x") ? "player2" : "player1";
+		console.log(player, gameController.getTurn())
 		const rows = [];
+		const moves = boardArray.filter((el) => el != 0);
+
 		for (let i = 3; i <= boardArray.length; i += 3) {
 			rows.push(boardArray.slice(i - 3, i));
 		}
@@ -65,27 +72,31 @@ const gameBoard = (function () {
 				diag1 === winCondition ||
 				diag2 === winCondition
 			) {
-				gameController.createOverlay();
-				return 1;
+				winner = player;
+				displayController.createOverlay();
+				return;
 			}
 		}
-		return 0;
+
+		// If the length of the non 0 array is 9 that means all have been played, 
+        // and if the win condition didn't trigger then it's a tie
+		
+		if (moves.length === 9) {
+			winner = "tie";
+			displayController.createOverlay();
+		}
+
+		return;
 	}
 
-	function checkTie() {
-        // If the length of the non 0 array is 9 that means all have been played, 
-        // and if the win condition didn't trigger then the it's a tie
-		const moves = boardArray.filter((el) => el != 0);
-		if (moves.length === 9) {
-			gameController.createOverlay(true);
-			return 1;
-		}
-		return 0;
+	function getWinner(){
+		return winner;
 	}
 
 	return {
 		init,
 		resetBoard,
+		getWinner
 	};
 })();
 
@@ -116,7 +127,7 @@ const gameController = (function () {
 	const resetBtn = document.querySelector("#reset-btn");
 	const setPlayer1btn = document.querySelector("#player1-input button");
 	const setPlayer2btn = document.querySelector("#player2-input button");
-	const overlay = document.querySelector(".overlay");
+
 	const player1 = { name: "Player 1", sym: "o" };
 	const player2 = { name: "Player 2", sym: "x" };
 
@@ -126,10 +137,7 @@ const gameController = (function () {
 
 	function addListeners() {
 		resetBtn.addEventListener("click", reset);
-		overlay.addEventListener("click", function () {
-			removeOverlay();
-			nextRound(overlay.getAttribute("data-tie"));
-		});
+
 		setPlayer1btn.addEventListener("click", function (e) {
 			reset();
 			createPlayer(e, player1);
@@ -147,39 +155,20 @@ const gameController = (function () {
 	}
 
 	function reset() {
-		removeOverlay();
+		displayController.removeOverlay();
 		gameBoard.resetBoard();
 		createPlayer(undefined, player1);
 		createPlayer(undefined, player2);
-		updateScore();
+		displayController.updateScore();
 		turn = "o";
-	}
-
-	function createOverlay(tie) {
-		overlay.style.display = "flex";
-		if (tie) {
-			overlay.firstChild.textContent = "Tie!";
-            // Adds the data attribute for checking the tie later without the 
-            // need to pass it as an argument
-			overlay.setAttribute("data-tie", 1);
-		} else {
-			if (getTurn() === "o") {
-				overlay.firstChild.textContent = `${player1.name} wins!`;
-			} else {
-				overlay.firstChild.textContent = `${player2.name} wins!`;
-			}
-		}
-	}
-
-	function removeOverlay() {
-		overlay.style.display = "none";
 	}
 
 	function getTurn() {
 		return turn;
 	}
+
 	function changeTurn() {
-		turn = turn === "x" ? "o" : "x";
+		turn = (turn === "x") ? "o" : "x";
 	}
 
 	function createPlayer(event, player) {
@@ -201,33 +190,73 @@ const gameController = (function () {
 		});
 	}
 
-	function nextRound(tie) {
-		gameBoard.resetBoard();
-		if (!tie) {
-			if (getTurn() === "o") {
+	function nextRound() {
+		if (!(gameBoard.getWinner() === "tie")) {
+			if (gameBoard.getWinner() === "player1") {
 				player1.increaseScore();
-			} else {
+			} else if (gameBoard.getWinner() === "player2") {
 				player2.increaseScore();
 			}
 		}
-		updateScore();
+		gameBoard.resetBoard();
+		displayController.updateScore();
 		turn = "o";
+	}
+
+	function getPlayers(){
+		return [player1, player2];
+	}
+
+	return {
+		addListeners,
+		nextRound,
+		getTurn,
+		changeTurn,
+		getPlayers
+	};
+})();
+
+const displayController = (function(){
+	const overlay = document.querySelector(".overlay");
+
+	addListeners();
+
+	function removeOverlay() {
+		overlay.style.display = "none";
+	}
+
+	function createOverlay() {
+		overlay.style.display = "flex";
+		if (gameBoard.getWinner() === "tie") {
+			overlay.firstChild.textContent = "Tie!";
+		} else {
+			if (gameBoard.getWinner() === "player1") {
+				overlay.firstChild.textContent = `${gameController.getPlayers()[0].name} wins!`;
+			} else {
+				overlay.firstChild.textContent = `${gameController.getPlayers()[1].name} wins!`;
+			}
+		}
 	}
 
 	function updateScore() {
 		const player1Score = document.querySelector("#counter-player1");
 		const player2Score = document.querySelector("#counter-player2");
-		player1Score.textContent = player1.getScore();
-		player2Score.textContent = player2.getScore();
+		player1Score.textContent = gameController.getPlayers()[0].getScore();
+		player2Score.textContent = gameController.getPlayers()[1].getScore();
+	}
+
+	function addListeners(){
+		overlay.addEventListener("click", function () {
+			removeOverlay();
+			gameController.nextRound();
+		});
 	}
 
 	return {
-		addListeners,
+		removeOverlay,
 		createOverlay,
-		nextRound,
-		getTurn,
-		changeTurn,
-	};
+		updateScore
+	}
 })();
 
 gameController.addListeners();
